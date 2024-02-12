45add09fe5db5f0464fcab358b9660fc10f846f0
Landi <landistover@gmail.com>
2024-02-12 18:35:08 +0200

    vita init

---
+++ src/interfaces/Person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+    FirstName:string;
+    NickName:string;
+    Surname:string;
+    IDNumber:string;
+    CellphoneNumber:string;
+    Username:string;
+    Password:string;
+}
---
+++ src/services/Person.ts
@@ -0,0 +1,13 @@
+import type { Person } from "../interfaces/Person";
+
+let people: Person[] = [];
+
+export function savePerson(person: Person) {
+  people.push(person);
+  localStorage.setItem('people', JSON.stringify(people));
+}
+
+export function getPerson(Username: string) : Person{
+    return JSON.parse(localStorage.getItem('people')!)
+    .find((p: Person) => p.Username === Username);
+}
